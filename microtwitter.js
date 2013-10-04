02b7703704d68bc1b914cc5d61b3d8a6b06af3e6
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2013-10-04 14:47:45 -0300

    Usando template.find

--- microtwitter.js
+++ microtwitter.js
@@ -2,9 +2,9 @@ Mensagem = new Meteor.Collection("mensagem");
 
 if (Meteor.isClient) {
   Template.formulario.events({
-    'click #enviar': function(event) {
-      var nome = $('#nome').val();
-      var conteudo = $('#conteudo').val();
+    'click #enviar': function(event, template) {
+      var nome = template.find('#nome').value;
+      var conteudo = template.find('#conteudo').value;
       Mensagem.insert({nome: nome, conteudo: conteudo, data: new Date().toLocaleString()});
       event.preventDefault();
     }
